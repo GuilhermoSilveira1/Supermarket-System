41d37c4eced8f5c9f1faa99a2defc73e27815b6d
Guilhermo Silveira <153184158+GuilhermoSilveira1@users.noreply.github.com>
2024-06-20 21:41:44 -0300

    Update Recibo.java

--- Lista 4/Recibo.java
+++ Lista 4/Recibo.java
@@ -1,32 +1,16 @@
-import java.util.ArrayList;
-
 public class Recibo {
+
+    private static int idCounter = 1;
     private int ID;
-    private Empregado atendente;
-    private ArrayList<Produtos> produtos;
-    private double total;
-    private String pagamento;
-    private double valorPago;
-    private double troco;
+    private Venda venda;
 
     public Recibo() {
-
+        this.ID = idCounter++;
     }
 
-    public Recibo(int ID,
-            Empregado atendente,
-            ArrayList<Produtos> produtos,
-            double total,
-            String pagamento,
-            double valorPago,
-            double troco) {
-        this.ID = ID;
-        this.atendente = atendente;
-        this.produtos = produtos;
-        this.total = total;
-        this.pagamento = pagamento;
-        this.valorPago = valorPago;
-        this.troco = troco;
+    public Recibo(Venda venda) {
+        this.ID = idCounter++;
+        this.venda = venda;
     }
 
     // Getters & Setters
@@ -34,55 +18,12 @@ public class Recibo {
         return ID;
     }
 
-    public void setID(int iD) {
-        ID = iD;
+    public Venda getVenda() {
+        return venda;
     }
 
-    public Empregado getAtendente() {
-        return atendente;
-    }
-
-    public void setAtendente(Empregado atendente) {
-        this.atendente = atendente;
-    }
-
-    public ArrayList<Produtos> getProdutos() {
-        return produtos;
-    }
-
-    public void setProdutos(ArrayList<Produtos> produtos) {
-        this.produtos = produtos;
-    }
-
-    public double getTotal() {
-        return total;
-    }
-
-    public void setTotal(double total) {
-        this.total = total;
-    }
-
-    public String getPagamento() {
-        return pagamento;
-    }
-
-    public void setPagamento(String pagamento) {
-        this.pagamento = pagamento;
-    }
-
-    public double getValorPago() {
-        return valorPago;
-    }
-
-    public void setValorPago(double valorPago) {
-        this.valorPago = valorPago;
-    }
-
-    public double getTroco() {
-        return troco;
-    }
-
-    public void setTroco(double troco) {
-        this.troco = troco;
+    public void setVenda(Venda venda) {
+        this.venda = venda;
     }
 }
+
